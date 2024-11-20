81521083b64be11e6fa61c6d90290d74cce1375b
Simon Van Casteren <simon.van.casteren@gmail.com>
2024-11-20 11:51:51 +0100

    Update default.nix

--- pkgs/esp-idf/default.nix
+++ pkgs/esp-idf/default.nix
@@ -132,6 +132,8 @@ stdenv.mkDerivation rec {
 
     chown -R 1000 $out/.git
 
+    echo "${rev}" > $out/version.txt
+
     # Link the Python environment in so that:
     # - The setup hook can set IDF_PYTHON_ENV_PATH to it.
     # - In shell derivations, the Python setup hook will add the site-packages
